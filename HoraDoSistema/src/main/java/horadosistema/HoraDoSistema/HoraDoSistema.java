959df3d64610b6f44b6157009cdc8cc0367b7d4e
Rodrigo Borge <rodrigo@borge.com.br>
2024-03-22 14:50:56 -0300

    Exercicio aula #04 - Relógio do Sistema

---
+++ HoraDoSistema/src/main/java/horadosistema/HoraDoSistema/HoraDoSistema.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package horadosistema.HoraDoSistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Rodrigo Borge
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+    }
+}
