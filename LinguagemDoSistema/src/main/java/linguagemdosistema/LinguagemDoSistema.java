aff92afcf7248c35c2a13b4890e5c4f413d82d24
Rodrigo Borge <rodrigo@borge.com.br>
2024-03-22 15:15:22 -0300

    Exercicio aula  #04 - Idioma do Sistema

---
+++ LinguagemDoSistema/src/main/java/linguagemdosistema/LinguagemDoSistema.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package linguagemdosistema;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Rodrigo Borge
+ */
+public class LinguagemDoSistema {
+
+    public static void main(String[] args) {
+        Locale idioma = Locale.getDefault();
+        System.out.println("O idioma do sistema é o: "idioma);
+        System.out.println(idioma);
+    }
+}
